b414cbc9807d8104f518c8beb8b4970343bdc498
matusfelipe <matusfelipe@gmail.com>
2022-04-21 14:45:56 -0400

    ✨ effects remade

--- src/store/files/__path__/__name@dasherize__/effects/__name@dasherize__.effects.spec.ts
+++ src/store/files/__path__/__name@dasherize__/effects/__name@dasherize__.effects.spec.ts
@@ -1,61 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-import { MatSnackBar, MatSnackBarDismiss } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
 import { provideMockActions } from '@ngrx/effects/testing';
-import { cold, hot } from 'jest-marbles';
-import { map, of, ReplaySubject } from 'rxjs';
+import { map, of } from 'rxjs';
 
-import * as <%=classify(name)%>Actions from '../actions/<%=dasherize(name)%>.actions';
-
-import {
-  mock<%=classify(name)%>,
-  mock<%=classify(name)%>Payload,
-} from '@payments/models/<%=dasherize(name)%>/<%=dasherize(name)%>.mocks';
-
-import { <%=classify(name)%>Repository } from '@data/repository/<%=dasherize(name)%>/<%=dasherize(name)%>.repository';
 import { <%=classify(name)%>Effects } from './<%=dasherize(name)%>.effects';
-import { mockApiError } from 'app/shared/mocks/api-error.mocks';
 import { Actions } from '@ngrx/effects';
-import { <%=camelize(name)%>RepositoryFactory } from 'app/data/repository/<%=dasherize(name)%>/<%=dasherize(name)%>.repository.factory';
-import { mockPaginationRequestParameters } from 'app/modules/shared/models/mocks';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { %=camelize(name)%>Actions } from '../actions/<%=dasherize(name)%>.actions';
 
 describe('<%=classify(name)%>Effects', () => {
   let effects: <%=classify(name)%>Effects;
-  let actions;
-  let router: Router;
-  let snackbar: MatSnackBar;
-  let <%=camelize(name)%>Service: <%=classify(name)%>Repository;
+  let repository: FeatureRepository;
   let failureObservable;
   let instanceEffects: (currentAction: any) => <%=classify(name)%>Effects;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [],
+      imports: [HttpClientTestingModule],
       providers: [
         <%=classify(name)%>Effects,
-        provideMockActions(() => actions),
-        {
-          provide: <%=classify(name)%>Repository,
-          useFactory: <%=camelize(name)%>RepositoryFactory,
-        },
-        {
-          provide: MatSnackBar,
-          useValue: {
-            open: jest.fn(() => ({
-              onAction: jest.fn(() => of()),
-              afterDismissed: jest.fn(() => of({ dismissedByAction: true } as MatSnackBarDismiss)),
-            })),
-          },
-        },
-        { provide: Router, useValue: { navigate: jest.fn() } },
+        provideMockActions(() => null),
+        { provide: FeatureRepository, useFactory: FeatureFactory },
       ],
     });
-    router = TestBed.inject(Router);
     effects = TestBed.inject(<%=classify(name)%>Effects);
-    snackbar = TestBed.inject(MatSnackBar);
-    <%=camelize(name)%>Service = TestBed.inject(<%=classify(name)%>Repository);
+    repository = TestBed.inject(FeatureRepository);
     instanceEffects = (currentAction): <%=classify(name)%>Effects =>
-      new <%=classify(name)%>Effects(currentAction, <%=camelize(name)%>Service, snackbar, router);
+      new <%=classify(name)%>Effects(currentAction, repository);
     failureObservable = of(null).pipe(
       map(() => {
         throw Error('fail');
@@ -63,128 +33,29 @@ describe('<%=classify(name)%>Effects', () => {
     );
   });
 
-  it('should create<%=classify(name)%> action activate create<%=classify(name)%>$ and create<%=classify(name)%> effects', () => {
-    const action = <%=classify(name)%>Actions.create<%=classify(name)%>({
-      templatePayload: mock<%=classify(name)%>Payload,
-    });
-    const completition = <%=classify(name)%>Actions.create<%=classify(name)%>Success({
-      <%=camelize(name)%>: mock<%=classify(name)%>,
-    });
-
-    actions = hot('--a-', { a: action });
-    const expected = cold('--b', { b: completition });
-
-    expect(effects.create<%=classify(name)%>$).toBeObservable(expected);
-  });
-
-  it('should create<%=classify(name)%> action activate create<%=classify(name)%>Error$ effect', done => {
-    <%=camelize(name)%>Service.create<%=classify(name)%> = () => failureObservable;
-    const currentAction = new Actions(of(<%=classify(name)%>Actions.create<%=classify(name)%>));
-    const effect = instanceEffects(currentAction);
-    const { create<%=classify(name)%>$ } = effect;
-
-    create<%=classify(name)%>$.subscribe(({ type }) => {
-      expect(type).toEqual(<%=classify(name)%>Actions.create<%=classify(name)%>Error.type);
-      done();
-    });
-  });
-
-  it('should open snackbar on create<%=classify(name)%>Error', done => {
-    const currentAction = new Actions(of(<%=classify(name)%>Actions.create<%=classify(name)%>Error));
-    const effect = instanceEffects(currentAction);
-    const { create<%=classify(name)%>Error$ } = effect;
-
-    create<%=classify(name)%>Error$.subscribe(() => {
-      expect(snackbar.open).toHaveBeenCalledWith('Ocurrió un error');
-      done();
-    });
-  });
-
-  it('should create<%=classify(name)%> action activate create<%=classify(name)%>Success$ effect', done => {
-    const action = <%=classify(name)%>Actions.create<%=classify(name)%>Success({
-      <%=camelize(name)%>: mock<%=classify(name)%>,
-    });
-    actions = new ReplaySubject(1);
-    actions.next(action);
-
-    effects.create<%=classify(name)%>Success$.subscribe(() => {
-      expect(snackbar.open).toHaveBeenCalledWith('Plantilla Creada', 'VER', {
-        duration: 5000,
-      });
-      expect(router.navigate).toHaveBeenCalledWith(['/<%=dasherize(name)%>/', mock<%=classify(name)%>.id]);
-      done();
-    });
-  });
-
-  it('should get<%=classify(name)%>ById action activate get<%=classify(name)%>ById$ and get<%=classify(name)%>ByIdSuccess effect', () => {
-    const action = <%=classify(name)%>Actions.get<%=classify(name)%>ById({
-      id: 'id_test',
-    });
-    const completition = <%=classify(name)%>Actions.get<%=classify(name)%>ByIdSuccess({
-      <%=camelize(name)%>: mock<%=classify(name)%>,
-    });
-
-    actions = hot('--a-', { a: action });
-    const expected = cold('--b', { b: completition });
-
-    expect(effects.get<%=classify(name)%>ById$).toBeObservable(expected);
-  });
-
-  it('should get<%=classify(name)%>ById action activate get<%=classify(name)%>ById$ and get<%=classify(name)%>ByIdError effect', done => {
-    const payload = { id: 'id_test_error' };
-    <%=camelize(name)%>Service.get<%=classify(name)%> = () => failureObservable;
-    const currentAction = new Actions(of(<%=classify(name)%>Actions.get<%=classify(name)%>ById(payload)));
-    const effect = instanceEffects(currentAction);
-    const { get<%=classify(name)%>ById$ } = effect;
-
-    get<%=classify(name)%>ById$.subscribe(({ type }) => {
-      expect(type).toEqual(<%=classify(name)%>Actions.get<%=classify(name)%>ByIdError.type);
-      done();
-    });
-  });
-
-  it('should get<%=classify(name)%>ByIdError action activate get<%=classify(name)%>ByIdError$ effect', done => {
-    const action = <%=classify(name)%>Actions.get<%=classify(name)%>ByIdError({ error: mockApiError });
-    actions = new ReplaySubject(1);
-    actions.next(action);
-
-    effects.get<%=classify(name)%>ByIdError$.subscribe(() => {
-      expect(snackbar.open).toHaveBeenCalledWith('Ocurrió un error');
-      done();
-    });
-  });
-
-  it('should get<%=classify(name)%>s action activate get<%=classify(name)%>s$ and get<%=classify(name)%>sSuccess effect', done => {
+  test('<%=camelize(name)%>$ effect response type should be equal to <%=camelize(name)%>Success type', done => {
     const currentAction = new Actions(
-      of(
-        <%=classify(name)%>Actions.get<%=classify(name)%>List({
-          parameters: mockPaginationRequestParameters,
-        }),
-      ),
+      of(%=camelize(name)%>Actions.<%=camelize(name)%>({ payload: mockUserPayload })),
     );
     const effect = instanceEffects(currentAction);
-    const { get<%=classify(name)%>List$ } = effect;
+    const { <%=camelize(name)%>$ } = effect;
 
-    get<%=classify(name)%>List$.subscribe(({ type }) => {
-      expect(type).toEqual(<%=classify(name)%>Actions.get<%=classify(name)%>ListSuccess.type);
+    <%=camelize(name)%>$.subscribe(({ type }) => {
+      expect(type).toEqual(%=camelize(name)%>Actions.<%=camelize(name)%>Success.type);
       done();
     });
   });
 
-  it('should get<%=classify(name)%>s action activate get<%=classify(name)%>s$ and get<%=classify(name)%>sFailure effects', done => {
-    <%=camelize(name)%>Service.get<%=classify(name)%>eList = () => failureObservable;
+  test('<%=camelize(name)%>$ effect response type should be equal to <%=camelize(name)%>Failure type', done => {
+    repository.<%=camelize(name)%> = () => failureObservable;
     const currentAction = new Actions(
-      of(
-        <%=classify(name)%>Actions.get<%=classify(name)%>List({
-          parameters: mockPaginationRequestParameters,
-        }),
-      ),
+      of(%=camelize(name)%>Actions.<%=camelize(name)%>({ payload: mockUserPayload })),
     );
     const effect = instanceEffects(currentAction);
-    const { get<%=classify(name)%>List$ } = effect;
+    const { <%=camelize(name)%>$ } = effect;
 
-    get<%=classify(name)%>List$.subscribe(({ type }) => {
-      expect(type).toEqual(<%=classify(name)%>Actions.get<%=classify(name)%>ListFailure.type);
+    <%=camelize(name)%>$.subscribe(({ type }) => {
+      expect(type).toEqual(%=camelize(name)%>Actions.<%=camelize(name)%>Failure.type);
       done();
     });
   });
--- src/store/files/__path__/__name@dasherize__/effects/__name@dasherize__.effects.ts
+++ src/store/files/__path__/__name@dasherize__/effects/__name@dasherize__.effects.ts
@@ -1,107 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { MatSnackBar, MatSnackBarDismiss } from '@angular/material/snack-bar';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-
+]
 import { of } from 'rxjs';
 import { catchError, concatMap, map, tap } from 'rxjs/operators';
-
-import * as <%=classify(name)%>Actions from '../actions/<%=dasherize(name)%>.actions';
-import { <%=classify(name)%> } from '@payments/models';
-import { <%=classify(name)%>Repository } from 'app/data/repository/<%=dasherize(name)%>/<%=dasherize(name)%>.repository';
+import { <%=camelize(name)%>Actions } from '../actions/<%=dasherize(name)%>.actions';
 
 @Injectable()
-export class <%=classify(name)%>Effects {
-  constructor(
-    private actions$: Actions,
-    private <%=camelize(name)%>Service: <%=classify(name)%>Repository,
-    private snackbar: MatSnackBar,
-    private router: Router,
-  ) {}
+export class <%=camelize(name)%>Effects {
+  constructor(private actions$: Actions, private repository: FeatureRepository) {}
 
-  create<%=classify(name)%>$ = createEffect(() =>
+  <%=camelize(name)%>$ = createEffect(() =>
     this.actions$.pipe(
-      ofType(<%=classify(name)%>Actions.create<%=classify(name)%>),
+      ofType(<%=camelize(name)%>Actions.<%=camelize(name)%>),
       concatMap(action =>
-        this.<%=camelize(name)%>Service.create<%=classify(name)%>(action.templatePayload).pipe(
-          map((response: <%=classify(name)%>) =>
-          <%=classify(name)%>Actions.create<%=classify(name)%>Success({
-            <%=camelize(name)%>: response,
-            }),
-          ),
-          catchError(error => of(<%=classify(name)%>Actions.create<%=classify(name)%>Error(error))),
-        ),
-      ),
-    ),
-  );
-
-  create<%=classify(name)%>Success$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(<%=classify(name)%>Actions.create<%=classify(name)%>Success),
-        concatMap(({ <%=camelize(name)%> }) =>
-          this.snackbar
-            .open('Plantilla Creada', 'VER', { duration: 5000 })
-            .afterDismissed()
-            .pipe(
-              tap((event: MatSnackBarDismiss) => {
-                if (event.dismissedByAction) {
-                  this.router.navigate(['/payment-template/', <%=camelize(name)%>.id]);
-                }
-              }),
-            ),
-        ),
-      ),
-    { dispatch: false },
-  );
-
-  create<%=classify(name)%>Error$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(<%=classify(name)%>Actions.create<%=classify(name)%>Error),
-        tap(() => this.snackbar.open('Ocurrió un error')),
-      ),
-    { dispatch: false },
-  );
-
-  get<%=classify(name)%>ById$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(<%=classify(name)%>Actions.get<%=classify(name)%>ById),
-      concatMap(({ id }) =>
-        this.<%=camelize(name)%>Service.get<%=classify(name)%>(id).pipe(
-          map((response: <%=classify(name)%>) =>
-            <%=classify(name)%>Actions.get<%=classify(name)%>ByIdSuccess({
-              <%=camelize(name)%>: response,
-            }),
-          ),
-          catchError(error => of(<%=classify(name)%>Actions.get<%=classify(name)%>ByIdError(error))),
-        ),
-      ),
-    ),
-  );
-
-  get<%=classify(name)%>ByIdError$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(<%=classify(name)%>Actions.get<%=classify(name)%>ByIdError),
-        tap(() => {
-          this.snackbar.open('Ocurrió un error');
-        }),
-      ),
-    { dispatch: false },
-  );
-
-  get<%=classify(name)%>List$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(<%=classify(name)%>Actions.get<%=classify(name)%>List),
-      concatMap(({ parameters }) =>
-        this.<%=camelize(name)%>Service.get<%=classify(name)%>List(parameters).pipe(
-          map(response =>
-            <%=classify(name)%>Actions.get<%=classify(name)%>ListSuccess({
-              <%=camelize(name)%>Collection: response,
-            }),
-          ),
-          catchError(error => of(<%=classify(name)%>Actions.get<%=classify(name)%>ListFailure(error))),
+        this.repository.<%=camelize(name)%>(action.payload).pipe(
+          map(() => <%=camelize(name)%>Actions.<%=camelize(name)%>Success()),
+          catchError(error => of(<%=camelize(name)%>Actions.<%=camelize(name)%>Failure(error))),
         ),
       ),
     ),
